48a7ad1d10e492cac6e16c94a7257deb4d263ec7
Flaviu Cojocaru <flaviucojocaru99@gmail.com>
2023-05-10 21:13:23 +0300

    fix[be]: update user serializer to delete/create a client instance if role is changed

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -14,25 +14,55 @@ class UserSerializer(serializers.Serializer):
     def is_client(self, instance):
         return instance.role.name == 'CLIENT'
     
+    def is_admin(self, instance):
+        return instance.role.name == 'ADMINISTRATOR'
+    
     def update(self, instance, validated_data):
+        client_instance = None
+        print("in update")
+        
         for prop, value in validated_data.items():
-            _instance = instance
+            print(f"{prop}:{value}")
             
-            if prop == 'role': value = Role.objects.get(name=value.upper()) 
-            if self.is_client(_instance) and prop == 'consumer_number': 
-                _instance = Client.objects.get(user=instance)            
-            
-            setattr(_instance, prop, value)
-            _instance.save()
-            
+            if prop == 'role':
+                if value.upper() == 'CLIENT':
+                    # if the new value for 'role' is "CLIENT'
+                    # create a new Client instance if one doesn't already exist
+                    queryset = Client.objects.filter(user=instance)
+                    if not queryset.exists():
+                        client_instance = Client(user=instance)
+                
+                if value.upper() == "ADMINISTRATOR":
+                    # if the new value for 'role' is 'ADMINISTRATOR'
+                    # delete any existing Client instance for this user
+                    Client.objects.filter(user=instance).delete()
+                
+                # set the value with the model instance of the role
+                value = Role.objects.get(name=value.upper())
+                
+            if self.is_client(instance) and prop == 'consumer_number':
+                print("Aicii")
+                if not client_instance:
+                    client_instance = Client.objects.get(user=instance)
+                setattr(client_instance, prop, value)
+                                
+            setattr(instance, prop, value)
+        
+        instance.save()
+        if client_instance: client_instance.save()
         return instance
     
     def to_representation(self, instance):
-        ret = super().to_representation(instance)
+        representation = super().to_representation(instance)
+        
+        if self.is_admin(instance):
+            representation.pop('consumer_number')
+            
         if self.is_client(instance):
-            ret['consumer_number'] = Client.objects.get(user=instance).consumer_number
-        return ret
-    
+            representation['consumer_number'] = Client.objects.get(user=instance).consumer_number
+        
+        return representation
+       
     # validators
     def validate_role(self, value):
         for role in Role.ROLE_CHOICES:
