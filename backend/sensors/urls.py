08ff7588f0451e8b3ec9f7a00233fbdef726e005
Flaviu Cojocaru <flaviucojocaru99@gmail.com>
2023-08-31 00:24:31 +0300

    fix[be]: fix the list and create measurements apis

--- backend/sensors/urls.py
+++ backend/sensors/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('sensors', views.SensorListCreateView.as_view()),
     path('sensors/<int:pk>', views.SensorRetrieveUpdateDestroyView.as_view()),
-    path('sensors/measurements', views.MeasurementListView.as_view()),
+    path('sensors/<int:sensor_id>/measurements', views.MeasurementListCreateView.as_view()),
 ]
--- backend/sensors/views.py
+++ backend/sensors/views.py
@@ -3,28 +3,26 @@ from rest_framework.permissions import IsAuthenticated
 
 from .serializers import SensorSerializer, MeasurementSerializer
 from .models import Sensor, Measurement
-from users.models import Client
-from devices.models import Device
 
 
 class SensorListCreateView(generics.ListCreateAPIView):
     serializer_class = SensorSerializer
     queryset = Sensor.objects.all()
-    
+
+
 class SensorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SensorSerializer
     queryset = Sensor.objects.all()
-    
-class MeasurementListView(generics.ListAPIView):
+
+
+class MeasurementListCreateView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = MeasurementSerializer
-    
+
     def get_queryset(self):
         """
         Return a queryset containing all measurements belonging to the authenticated client.
         """
-        owner = Client.objects.get(user=self.request.user)
-        devices = Device.objects.filter(client=owner)
-        sensors = Sensor.objects.filter(device__in = devices)
-        measurements = Measurement.objects.filter(sensor__in = sensors)
+        sensor = self.kwargs["sensor_id"]
+        measurements = Measurement.objects.filter(sensor=sensor)
         return measurements
